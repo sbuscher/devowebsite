b883d7c3e060e1c7f28182a1628ef36c905f50f5
shane <shane@buscherhood.com>
2018-10-05 23:04:21 -0500

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent},
+  { path: "curriculum", component: CurriculumComponent},
+  { path: "gallery", component: GalleryComponent},
+  { path: "schedule", component: ScheduleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ScheduleComponent,
+    CurriculumComponent,
+    GalleryComponent,
+    VolunteerComponent,
+    RegistrationComponent,
+    AboutComponent,
+    ContactComponent,
+    SponsorsComponent
   ],
   imports: [
     BrowserModule,
